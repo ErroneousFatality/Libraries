7ce1ee3afbe02099ba96bb44ce256a35e41328b0
Andrej Križan <andrej_so@hotmail.com>
2023-01-25 17:19:24 +0100

    Common.Extensions: ImmutableArray.Convert - added a simple summary documentation.

--- Common/Extensions/ImmutableArrayExtensions.cs
+++ Common/Extensions/ImmutableArrayExtensions.cs
@@ -4,6 +4,7 @@ namespace AndrejKrizan.Common.Extensions
 {
     public static class ImmutableArrayExtensions
     {
+        /// <summary>Efficiently projects an <see cref="ImmutableArray{T}"/> to another.</summary>
         public static ImmutableArray<TResult> Convert<T, TResult>(this ImmutableArray<T> items, Func<T, TResult> converter)
             => ImmutableArray.CreateRange(items, converter);
     }
