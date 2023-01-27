dcb9b71caea28328b2c352953fd6bf20463eccb6
Andrej Križan <andrej_so@hotmail.com>
2023-01-27 03:45:33 +0100

    Hdf: refactored extensions

--- Hdf/Extensions/IntExtensions.cs
+++ Hdf/Extensions/IntExtensions.cs
@@ -1,29 +1,18 @@
 ﻿namespace AndrejKrizan.Hdf.Extensions
 {
-    internal static class HdfExtensions
+    internal static class IntExtensions
     {
-        public static bool IsValidHdfId(this long response)
-            => response >= HdfConstants.ResponseErrorCode;
         public static bool IsValidHdfResponse(this int response)
             => response >= HdfConstants.ResponseErrorCode;
 
-        public static long ValidateHdfId(this long response, string action)
-            => response.IsValidHdfId()
-                ? response
-                : throw new ApplicationException($"Failed to {action}. Response: {response}.");
         public static int ValidateHdfResponse(this int response, string action)
             => response.IsValidHdfResponse()
                 ? response
                 : throw new ApplicationException($"Failed to {action}. Response: {response}.");
 
-        public static long ValidateHdfId(this long response, Func<string> actionFunc)
-            => response.IsValidHdfId()
-                ? response
-                : throw new ApplicationException($"Failed to {actionFunc()}. Response: {response}.");
         public static int ValidateHdfResponse(this int response, Func<string> actionFunc)
             => response.IsValidHdfResponse()
                 ? response
                 : throw new ApplicationException($"Failed to {actionFunc()}. Response: {response}.");
-
     }
 }
---
+++ Hdf/Extensions/LongExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace AndrejKrizan.Hdf.Extensions
+{
+    internal static class LongExtensions
+    {
+        public static bool IsValidHdfId(this long response)
+            => response >= HdfConstants.ResponseErrorCode;
+
+        public static long ValidateHdfId(this long response, string action)
+            => response.IsValidHdfId()
+                ? response
+                : throw new ApplicationException($"Failed to {action}. Response: {response}.");
+
+        public static long ValidateHdfId(this long response, Func<string> actionFunc)
+            => response.IsValidHdfId()
+                ? response
+                : throw new ApplicationException($"Failed to {actionFunc()}. Response: {response}.");
+    }
+}
