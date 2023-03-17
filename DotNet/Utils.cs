ab01d2ae12e286782fe03d0deee7959fce57abc7
Andrej Križan <andrej_so@hotmail.com>
2023-03-17 13:17:02 +0100

    DotNet: Utils.OrAsync - now supports external cancellation and also internally cancels all remaining Tasks when one Task returns true.

--- DotNet/Utils.cs
+++ DotNet/Utils.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using System.Net.Mail;
 
 namespace AndrejKrizan.DotNet
 {
@@ -122,32 +121,127 @@ namespace AndrejKrizan.DotNet
             => Max(first, second, Comparer<T>.Default);
 
         #region OrAsync
-        public static async Task<bool> OrAsync(IEnumerable<Task<bool>> conditionTasks)
+
+#pragma warning disable CA1068 // CancellationToken parameters must come last
+        public static Task<bool> OrAsync(
+            CancellationToken cancellationToken, 
+            Func<CancellationToken, Task<bool>> condition1, 
+            Func<CancellationToken, Task<bool>> condition2, 
+            params Func<CancellationToken, Task<bool>>[] additionalConditions
+        )
+            => OrAsync(additionalConditions.Prepend(condition2).Prepend(condition1), cancellationToken);
+#pragma warning restore CA1068 // CancellationToken parameters must come last
+
+        public static Task<bool> OrAsync(
+            Func<CancellationToken, Task<bool>> condition1, 
+            Func<CancellationToken, Task<bool>> condition2, 
+            params Func<CancellationToken, Task<bool>>[] additionalConditions
+        )
+            => OrAsync(additionalConditions.Prepend(condition2).Prepend(condition1));
+
+        public static Task<bool> OrAsync(IEnumerable<Func<CancellationToken, Task<bool>>> conditions, CancellationToken cancellationToken = default)
+            => OrAsyncInternal(
+                (CancellationToken cancellationToken) => conditions.Select(
+                    conditionTaskFunc => Task.Run(() => conditionTaskFunc(cancellationToken), cancellationToken)
+                ),
+                cancellationToken
+            );
+
+
+#pragma warning disable CA1068 // CancellationToken parameters must come last
+        public static Task<bool> OrAsync(
+            CancellationToken cancellationToken,
+            Func<Task<bool>> condition1,
+            Func<Task<bool>> condition2,
+            params Func<Task<bool>>[] additionalConditions
+        )
+            => OrAsync(additionalConditions.Prepend(condition2).Prepend(condition1), cancellationToken);
+#pragma warning restore CA1068 // CancellationToken parameters must come last
+
+        public static Task<bool> OrAsync(
+            Func<Task<bool>> condition1,
+            Func<Task<bool>> condition2,
+            params Func<Task<bool>>[] additionalConditions
+        )
+            => OrAsync(additionalConditions.Prepend(condition2).Prepend(condition1));
+
+        public static Task<bool> OrAsync(IEnumerable<Func<Task<bool>>> conditions, CancellationToken cancellationToken = default)
+            => OrAsyncInternal(
+                (CancellationToken cancellationToken) => conditions.Select(
+                    conditionTaskFunc => Task.Run(conditionTaskFunc, cancellationToken)
+                ),
+                cancellationToken
+            );
+
+
+#pragma warning disable CA1068 // CancellationToken parameters must come last
+        public static Task<bool> OrAsync(
+            CancellationToken cancellationToken,
+            Func<bool> condition1,
+            Func<bool> condition2,
+            params Func<bool>[] additionalConditions
+        )
+            => OrAsync(additionalConditions.Prepend(condition2).Prepend(condition1), cancellationToken);
+#pragma warning restore CA1068 // CancellationToken parameters must come last
+
+        public static Task<bool> OrAsync(
+            Func<bool> condition1,
+            Func<bool> condition2,
+            params Func<bool>[] additionalConditions
+        )
+            => OrAsync(additionalConditions.Prepend(condition2).Prepend(condition1));
+
+        public static Task<bool> OrAsync(IEnumerable<Func<bool>> conditions, CancellationToken cancellationToken = default)
+            => OrAsyncInternal(
+                (CancellationToken cancellationToken) => conditions.Select(
+                    conditionTaskFunc => Task.Run(conditionTaskFunc, cancellationToken)
+                ),
+                cancellationToken
+            );
+
+
+#pragma warning disable CA1068 // CancellationToken parameters must come last
+        public static Task<bool> OrAsync(
+            CancellationToken cancellationToken,
+            Task<bool> condition1,
+            Task<bool> condition2,
+            params Task<bool>[] additionalConditions
+        )
+            => OrAsync(additionalConditions.Prepend(condition2).Prepend(condition1), cancellationToken);
+#pragma warning restore CA1068 // CancellationToken parameters must come last
+
+        public static Task<bool> OrAsync(
+            Task<bool> condition1,
+            Task<bool> condition2,
+            params Task<bool>[] additionalConditions
+        )
+            => OrAsync(additionalConditions.Prepend(condition2).Prepend(condition1));
+
+        public static Task<bool> OrAsync(IEnumerable<Task<bool>> conditions, CancellationToken cancellationToken = default)
+            => OrAsyncInternal(
+                (CancellationToken cancellationToken) => conditions,
+                cancellationToken
+            );
+
+
+        private static async Task<bool> OrAsyncInternal(Func<CancellationToken, IEnumerable<Task<bool>>> createConditionTasks, CancellationToken cancellationToken = default)
         {
+            using CancellationTokenSource cancellationSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+            IEnumerable<Task<bool>> conditionTasks = createConditionTasks(cancellationSource.Token);
             HashSet<Task<bool>> conditionTaskSet = new(conditionTasks);
             while (conditionTaskSet.Count > 0)
             {
                 Task<bool> completedConditionTask = await Task.WhenAny(conditionTaskSet);
+                cancellationToken.ThrowIfCancellationRequested();
                 if (completedConditionTask.Result)
                 {
+                    cancellationSource.Cancel();
                     return true;
                 }
                 conditionTaskSet.Remove(completedConditionTask);
             }
             return false;
         }
-        public static Task<bool> OrAsync(Task<bool> conditionTask, params Task<bool>[] additionalConditionTasks)
-            => OrAsync(additionalConditionTasks.Prepend(conditionTask));
-
-        public static Task<bool> OrAsync(IEnumerable<Func<Task<bool>>> conditionAsyncFunctions)
-            => OrAsync(conditionAsyncFunctions.Select(Task.Run));
-        public static Task<bool> OrAsync(Func<Task<bool>> conditionAsyncFunction, params Func<Task<bool>>[] additionalConditionAsyncFunctions)
-            => OrAsync(additionalConditionAsyncFunctions.Prepend(conditionAsyncFunction));
-
-        public static Task<bool> OrAsync(IEnumerable<Func<bool>> conditionFunctions)
-            => OrAsync(conditionFunctions.Select(Task.Run));
-        public static Task<bool> OrAsync(Func<bool> conditionFunction, params Func<bool>[] additionalConditionFunctions)
-            => OrAsync(additionalConditionFunctions.Prepend(conditionFunction));
         #endregion
     }
 }
