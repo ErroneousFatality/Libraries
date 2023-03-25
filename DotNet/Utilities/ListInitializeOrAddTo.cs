abce2ef42b4b0f3e7c41774fed3259d7c8e18816
Andrej Križan <andrej_so@hotmail.com>
2023-03-25 22:47:55 +0100

    DotNet: Utils - renamed ListInitializeOrAdd to ListInitializeOrAddTo, added support for params and IEnumerable parameters and separated into separate partial class file.

---
+++ DotNet/Utilities/ListInitializeOrAddTo.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace AndrejKrizan.DotNet.Utilities
+{
+    public static partial class Utils
+    {
+        public static void ListInitializeOrAddTo<T>([NotNull] ref List<T>? list, T item)
+        {
+            if (list == null)
+            {
+                list = new List<T> { item };
+            }
+            else
+            {
+                list.Add(item);
+            }
+        }
+
+        public static void ListInitializeOrAddTo<T>([NotNull] ref List<T>? list, T item, params T[] additionalItems)
+            => ListInitializeOrAddTo(ref list, additionalItems.Prepend(item));
+
+        public static void ListInitializeOrAddTo<T>([NotNull] ref List<T>? list, IEnumerable<T> items)
+        {
+            if (list == null)
+            {
+                list = new List<T>(items);
+            }
+            else
+            {
+                list.AddRange(items);
+            }
+        }
+    }
+}
--- DotNet/Utilities/Utils.cs
+++ DotNet/Utilities/Utils.cs
@@ -1,21 +1,7 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace AndrejKrizan.DotNet.Utilities
+﻿namespace AndrejKrizan.DotNet.Utilities
 {
     public static partial class Utils
     {
-        public static void ListInitializeOrAdd<T>([NotNull] ref List<T>? list, T item)
-        {
-            if (list == null)
-            {
-                list = new List<T> { item };
-            }
-            else
-            {
-                list.Add(item);
-            }
-        }
-
         public static T Min<T>(T first, T second, IComparer<T> comparer)
             => comparer.Compare(first, second) <= 0 ? first : second;
         public static T Min<T>(T first, T second)
