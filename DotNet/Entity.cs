06cbeb22b610ca77f7345e85d0b2dcfa28b53b41
Andrej Križan <andrej_so@hotmail.com>
2023-04-06 01:27:40 +0200

    v1.0.14 - DotNet.Entity - added constructor that sets Id to Guid Entity class.

--- DotNet/Entity.cs
+++ DotNet/Entity.cs
@@ -25,6 +25,12 @@
     /// </summary>
     public abstract class Entity : Entity<Guid>
     {
+        // Constructors
+        protected Entity(Guid id)
+            : base(id) { }
 
+        /// <summary>Constructor for EntityFrameworkCore.</summary>
+        /// <remarks>EntityFrameworkCore chooses the constructor with the least amount of parameters (including 0) where all of them correspond to a property.</remarks>
+        protected Entity() { }
     }
 }
