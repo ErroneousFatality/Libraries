f9f6e55f7b5e72ce861ead756a72f3375ecfe52a
Andrej Križan <andrej_so@hotmail.com>
2023-04-06 12:06:17 +0200

    v1.0.15: DotNet - added EnumExtensions containing ToFlags method

---
+++ DotNet/Extensions/EnumExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace AndrejKrizan.DotNet.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static IEnumerable<TEnum> ToFlags<TEnum>(this TEnum flags)
+            where TEnum : struct, Enum
+            => Enum.GetValues<TEnum>().Where(e => flags.HasFlag(e));
+    }
+}
