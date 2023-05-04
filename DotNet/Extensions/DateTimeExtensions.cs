c4327fae7f26797f468d559ccfcfca9462ac4788
Andrej Križan <andrej_so@hotmail.com>
2023-05-04 13:37:49 +0200

    v1.0.21: DotNet: added DateTime extension methods: ToDateOnly and ToTimeOnly

--- DotNet/Extensions/DateTimeExtensions.cs
+++ DotNet/Extensions/DateTimeExtensions.cs
@@ -60,4 +60,7 @@ public static class DateTimeExtensions
     }
     public static int GetDaysSinceStartOfWeek(this DateTime dateTime)
         => dateTime.GetDaysSinceStartOfWeek(Thread.CurrentThread.CurrentCulture.DateTimeFormat.FirstDayOfWeek);
+
+    public static DateOnly ToDateOnly(this DateTime dateTime) => DateOnly.FromDateTime(dateTime);
+    public static TimeOnly ToTimeOnly(this DateTime dateTime) => TimeOnly.FromDateTime(dateTime);
 }
