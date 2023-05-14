430057ffbbcbebb2749d4df42f8d39ecbad5a418
Andrej Križan <andrej_so@hotmail.com>
2023-05-14 08:33:59 +0200

    DotNet: RandomExtensions.NextSet - improved documentation and added overload without size parameter.

--- DotNet/Extensions/RandomExtensions.cs
+++ DotNet/Extensions/RandomExtensions.cs
@@ -4,11 +4,13 @@ public static class RandomExtensions
     public static bool NextBool(this Random random, double truePercentage = 50f)
         => random.NextDouble() * 100d < truePercentage;
 
-    /// <remarks>Works in O(count) complexity.</remarks>
-    public static HashSet<int> NextSet(this Random random, int count, int min, int max)
+    #region NextSet
+    /// <returns>A set of random integers from [<paramref name="min"/>, <paramref name="max"/>) range.</returns>
+    /// <remarks>Works in O(<paramref name="size"/>) complexity.</remarks>
+    public static HashSet<int> NextSet(this Random random, int size, int min = 0, int max = int.MaxValue)
     {
         HashSet<int> set = new();
-        for (int limit = max - count + 1; limit <= max; limit++)
+        for (int limit = max - size + 1; limit <= max; limit++)
         {
             int value = random.Next(min, limit);
             if (set.Contains(value))
@@ -22,4 +24,9 @@ public static class RandomExtensions
         }
         return set;
     }
+
+    /// <returns>A random sized set of random integers from [<paramref name="min"/>, <paramref name="max"/>) range.</returns>
+    public static HashSet<int> NextSet(this Random random, int min = 0, int max = int.MaxValue)
+        => random.NextSet(random.Next(0, max - min + 1), min, max);
+    #endregion
 }
