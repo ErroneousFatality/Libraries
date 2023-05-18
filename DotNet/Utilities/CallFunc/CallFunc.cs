4e21fdd160aca4f7b1df400344b2dc70c360aa9d
Andrej Križan <andrej_so@hotmail.com>
2023-05-19 01:16:08 +0200

    DotNet: added Utils.CallFunc<T, TResult>() for calling instance methods like they're static.

---
+++ DotNet/Utilities/CallFunc/CallFunc.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+using AndrejKrizan.DotNet.Utilities.Func;
+
+namespace AndrejKrizan.DotNet.Utilities;
+
+public static partial class Utils
+{
+    // Methods
+    public static TResult CallFunc<T, TResult>(Func<T, TResult> func)
+        => func(Mock<T>());
+
+    public static TResult CallFunc<T, TResult>(string name, params object?[] parameters)
+        => CallFunc<T, TResult>(typeof(T).GetMethod(name, BindingFlags.NonPublic)!, parameters);
+
+    public static TResult CallFunc<T, TResult>(Expression<Func<T, Func<TResult>>> navigation, params object?[] parameters)
+        => CallFunc<T, TResult>(new MethodInfoFinder().FindIn(navigation), parameters);
+
+    public static TResult CallFunc<T, TResult>(MethodInfo info, params object?[] parameters)
+        => (TResult)info.Invoke(Mock<T>(), parameters)!;
+
+    // Private methods
+    private static T Mock<T>()
+        => (T)Activator.CreateInstance(typeof(T), nonPublic: true)!;
+
+}
---
+++ DotNet/Utilities/CallFunc/MethodInfoFinder.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace AndrejKrizan.DotNet.Utilities.Func;
+public class MethodInfoFinder : ExpressionVisitor
+{
+    // Methods
+    public MethodInfo FindIn(Expression expression)
+    {
+        Visit(expression);
+        return MethodInfo ?? throw new ArgumentException($"The expression {expression} contains no method information.");
+    }
+
+    // Protected methods
+    protected override Expression VisitConstant(ConstantExpression node)
+    {
+        if (node.Value is MethodInfo methodInfo)
+        {
+            if (MethodInfo != null)
+            {
+                throw new ArgumentException("The expression contains multiple method informations.");
+            }
+            MethodInfo = methodInfo;
+        }
+        return base.VisitConstant(node);
+    }
+
+    // Private fields
+    private MethodInfo? MethodInfo;
+}
