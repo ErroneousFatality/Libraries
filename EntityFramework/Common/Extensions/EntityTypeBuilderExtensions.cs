896191dd2ee9981fa23fbb3a9cdf2f38c9ece946
Andrej Križan <andrej_so@hotmail.com>
2023-05-29 19:31:19 +0200

    v1.0.41 EntityFramework.Common: bugfix for HasKey extension.

--- EntityFramework/Common/Extensions/EntityTypeBuilderExtensions.cs
+++ EntityFramework/Common/Extensions/EntityTypeBuilderExtensions.cs
@@ -1,10 +1,4 @@
-﻿using System.Collections.Immutable;
-using System.Linq.Expressions;
-using System.Reflection;
-using System.Reflection.Emit;
-
-using AndrejKrizan.DotNet.Entities;
-using AndrejKrizan.DotNet.ValueObjects.PropertyBindings;
+﻿using AndrejKrizan.DotNet.Entities;
 
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -15,33 +9,5 @@ public static class EntityTypeBuilderExtensions
     public static KeyBuilder HasKey<TEntity, TKey>(this EntityTypeBuilder<TEntity> entity)
         where TEntity : class
         where TKey: struct, IKey<TEntity, TKey>
-    {
-        ImmutableArray<IPropertyBinding<TEntity, TKey>> propertyBindings = IKey<TEntity, TKey>.PropertyBindings;
-        Type type = CreateAnonymousKeyType(propertyBindings);
-        ConstructorInfo constructor = type.GetConstructor(propertyBindings.Select(property => property.Type).ToArray())!;
-        IEnumerable<MemberExpression> arguments = propertyBindings.Select(property => (MemberExpression)property.EntityProperty.Expression);
-        Expression<Func<TEntity, object?>> construction = Expression.Lambda<Func<TEntity, object?>>(
-            Expression.New(constructor, arguments), 
-            IKey<TEntity, TKey>.EntityParameter
-        );
-        return entity.HasKey(construction);
-    }
-
-    // Private methods
-    private static Type CreateAnonymousKeyType<TEntity, TKey>(IEnumerable<IPropertyBinding<TEntity, TKey>> propertyBindings)
-        where TEntity: class
-        where TKey : struct, IKey<TEntity, TKey>
-    {
-        TypeBuilder builder = AssemblyBuilder
-            .DefineDynamicAssembly(new AssemblyName("DynamicAnonymousTypeCreator"), AssemblyBuilderAccess.RunAndCollect)
-            .DefineDynamicModule("DynamicAnonymousTypeCreator")
-            .DefineType(typeof(TKey).Name, TypeAttributes.Public);
-        foreach (IPropertyBinding<TEntity, TKey> propertyBinding in propertyBindings)
-        {
-            PropertyInfo property = propertyBinding.EntityProperty.PropertyInfo;
-            builder.DefineField(property.Name, property.PropertyType, FieldAttributes.Public);
-        }
-        Type type = builder.CreateType();
-        return type;
-    }
+        => entity.HasKey(IKey<TEntity, TKey>.PropertyBindings.Select(binding => binding.EntityProperty.PropertyInfo.Name).ToArray());
 }
