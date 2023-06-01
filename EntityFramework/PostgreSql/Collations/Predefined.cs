93da6952788d8468abd5cf34f8dacb990be1b88d
Andrej Križan <andrej_so@hotmail.com>
2023-06-01 12:46:33 +0200

    EntityFramework.PostgreSql: removed duplicate and impossible collations for unicode accent insensitive case insensitive.

--- EntityFramework/PostgreSql/Collations/Predefined.cs
+++ EntityFramework/PostgreSql/Collations/Predefined.cs
@@ -1,12 +1,6 @@
 ﻿namespace AndrejKrizan.EntityFramework.PostgreSql.Collations;
 public static class Predefined
 {
-    public static readonly Collation UnicodeAccentInsensitiveCaseInsensitive 
-        = new("unicode_accent_insensitive_case_insensitive", locale: "und-u-ks-level1", provider: "icu");
-
-    public static readonly Collation UnicodeAccentInsensitiveCaseInsensitiveUndeterministic
+    public static readonly Collation UnicodeAccentInsensitiveCaseInsensitive
         = new("unicode_accent_insensitive_case_insensitive_deterministic", locale: "und-u-ks-level1", provider: "icu", deterministic: false);
-
-    public static readonly Collation UnicodeAccentInsensitiveCaseInsensitiveDeterministic
-        = new("unicode_accent_insensitive_case_insensitive_undeterministic", locale: "und-u-ks-level1", provider: "icu", deterministic: true);
 }
