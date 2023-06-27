3bd145bc6f0b5dc45a1e0d9de5daa829840643b0
Andrej Križan <andrej_so@hotmail.com>
2023-06-27 15:04:15 +0200

    v1.0.45 EntityFramework.PostgreSql: fixed collation naming.

--- EntityFramework/PostgreSql/Collations/Predefined.cs
+++ EntityFramework/PostgreSql/Collations/Predefined.cs
@@ -2,5 +2,5 @@
 public static class Predefined
 {
     public static readonly Collation UnicodeAccentInsensitiveCaseInsensitive
-        = new("unicode_accent_insensitive_case_insensitive_deterministic", locale: "und-u-ks-level1", provider: "icu", deterministic: false);
+        = new("unicode_accent_insensitive_case_insensitive", locale: "und-u-ks-level1", provider: "icu", deterministic: false);
 }
