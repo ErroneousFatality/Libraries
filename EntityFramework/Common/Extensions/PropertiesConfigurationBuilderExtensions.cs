f009848d2baf6ad5a46d655fb3c465f380c4a4a2
Andrej Križan <andrej_so@hotmail.com>
2023-07-15 17:26:40 +0200

    v1.0.47 EntityFramework: added HaveJsonConvertsion() extension methods to properties configurator.

---
+++ EntityFramework/Common/Extensions/PropertiesConfigurationBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AndrejKrizan.EntityFramework.Common.Converters;
+
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AndrejKrizan.EntityFramework.Common.Extensions;
+public static class PropertiesConfigurationBuilderExtensions
+{
+    public static PropertiesConfigurationBuilder<TProperty> HaveJsonConversion<TProperty>(this PropertiesConfigurationBuilder<TProperty> property)
+        => property.HaveConversion<JsonConverter<TProperty>>();
+}
---
+++ EntityFramework/PostgreSql/Extensions/PropertiesConfigurationBuilderExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AndrejKrizan.EntityFramework.PostgreSql.Extensions;
+public static class PropertiesConfigurationBuilderExtensions
+{
+    public static PropertiesConfigurationBuilder<TProperty> HaveJsonConversion<TProperty>(this PropertiesConfigurationBuilder<TProperty> property)
+        => Common.Extensions.PropertiesConfigurationBuilderExtensions.HaveJsonConversion(property).HaveColumnType("jsonb");
+}
