d0c0bd9567acbc8ed395f5a42680172b66958a39
Andrej Križan <andrej_so@hotmail.com>
2023-08-02 10:33:56 +0200

    v1.0.54 DotNet:   - added nullable extension method: TryGetValue   - added overload for object extension method Apply which allows transformation.

---
+++ DotNet/Extensions/NullableExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace AndrejKrizan.DotNet.Extensions;
+public static class NullableExtensions
+{
+    public static bool TryGetValue<T> (this T? nullable, out T value)
+        where T: struct
+    {
+        if (nullable.HasValue)
+        {
+            value = nullable.Value;
+            return true;
+        }
+        value = default;
+        return false;
+    }
+}
--- DotNet/Extensions/ObjectExtensions.cs
+++ DotNet/Extensions/ObjectExtensions.cs
@@ -3,13 +3,16 @@
 public static class ObjectExtensions
 {
     #region Apply
-    public static T Apply<T>(this T value, Func<T, T> function)
-        => function(value);
-
     public static T Apply<T>(this T value, Action<T> action)
     {
         action(value);
         return value;
     }
+
+    public static T Apply<T>(this T value, Func<T, T> function)
+        => function(value);
+
+    public static TResult Apply<T, TResult>(this T value, Func<T, TResult> function)
+        => function(value);
     #endregion
 }
