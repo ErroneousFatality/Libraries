9a358f34605ebaa9eed2574d41614303abc72c89
Andrej Križan <andrej_so@hotmail.com>
2023-08-02 10:23:00 +0200

    v1.0.53 DotNet: added decimal extensions: decimal.ToDouble() and decimal?.ToDouble().

---
+++ DotNet/Extensions/DecimalExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace AndrejKrizan.DotNet.Extensions;
+public static class DecimalExtensions
+{
+    public static double ToDouble(this decimal value)
+        => decimal.ToDouble(value);
+
+    public static double? ToDouble(this decimal? value)
+        => value?.ToDouble();
+}
