7675634fedc6d53f0a76b543201e42298359a432
Andrej Križan <andrej_so@hotmail.com>
2023-09-07 10:23:55 +0200

    v1.0.56:   - DotNet: Moved EmailAddress validation code from Extensions to EmailAddress namespace.   - DotNet.EmailAddress + PhoneNumbers:       - Validation error exceptions no longer contain passed value (email address, phoneNumber).       - Validation methods now accept an "owner" string parameter to personalize the validation error.       - Optimized validation error creation.   - Updated nuget packages.

--- PhoneNumbers/Extensions/StringExtensions.cs
+++ PhoneNumbers/Extensions/StringExtensions.cs
@@ -1,19 +1,33 @@
-﻿using PhoneNumbers;
+﻿using System.Text;
+
+using PhoneNumbers;
 
 namespace AndrejKrizan.PhoneNumbers.Extensions;
 
 public static class StringExtensions
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="owner">Will be added to the validation error message.</param>
+    /// <param name="regionCode">ISO 3166 country alpha-2 code (<see href="https://www.iso.org/obp/ui/#search/code/"/>).</param>
+    /// <returns>A region formatted phone number string.</returns>
     /// <exception cref="ArgumentException"></exception>
-    public static string ToPhoneNumber(this string phoneNumber, PhoneNumberFormat format = PhoneNumberFormat.INTERNATIONAL, string? regionCode = null, int maxLength = PhoneNumberConstraints.MaxLength)
+    public static string ToPhoneNumber(this string phoneNumber, 
+        string? owner = null,
+        string? regionCode = null,
+        PhoneNumberFormat format = PhoneNumberFormat.INTERNATIONAL, 
+        int maxLength = PhoneNumberConstraints.MaxLength
+    )
     {
         if (string.IsNullOrWhiteSpace(phoneNumber))
         {
-            throw new ArgumentException($"The phone number must not be empty.", nameof(phoneNumber));
+            throw new ArgumentException($"{CreateName(owner)} must not be empty.", nameof(phoneNumber));
         }
+
         if (phoneNumber.Length > maxLength)
         {
-            throw new ArgumentException($"The phone number \"{phoneNumber}\" is too long. Maximum length: {maxLength}.", nameof(phoneNumber));
+            throw new ArgumentException($"{CreateName(owner)} is too long. Maximum length: {maxLength}.", nameof(phoneNumber));
         }
 
         PhoneNumberUtil utils = PhoneNumberUtil.GetInstance();
@@ -22,25 +36,47 @@ public static class StringExtensions
         {
             _phoneNumber = utils.Parse(phoneNumber, regionCode);
         }
-        catch (Exception exception)
+        catch (NumberParseException exception)
         {
-            throw new ArgumentException($"{CreatePhoneNumberValidationErrorMessage(phoneNumber, regionCode)}: {exception.Message}.", nameof(phoneNumber), exception);
+            if (exception.ErrorType == ErrorType.INVALID_COUNTRY_CODE)
+            {
+                throw new ArgumentOutOfRangeException($"The phone number region code \"{regionCode}\" is not valid.", exception);
+            }
+            throw new ArgumentException(CreateErrorMessage(owner, regionCode), nameof(phoneNumber), exception);
         }
+
         if (regionCode != null && !utils.IsValidNumberForRegion(_phoneNumber, regionCode))
         {
-            throw new ArgumentException(CreatePhoneNumberValidationErrorMessage(phoneNumber, regionCode) + '.', nameof(phoneNumber));
+            throw new ArgumentException(CreateErrorMessage(owner, regionCode), nameof(phoneNumber));
         }
+
         string validPhoneNumber = utils.Format(_phoneNumber, format);
         return validPhoneNumber;
     }
 
-    private static string CreatePhoneNumberValidationErrorMessage(string phoneNumber, string? regionCode)
+
+    // Private methods
+    private static string CreateErrorMessage(string? owner, string? regionCode)
     {
-        string errorMessage = $"The phone number \"{phoneNumber}\" is not valid";
+        StringBuilder builder = new(75);
+        builder.Append($"{CreateName(owner)} is not valid");
         if (regionCode != null)
         {
-            errorMessage += $" for region \"{regionCode}\"";
+            builder.Append($" for region \"{regionCode}\"");
         }
-        return errorMessage;
+        builder.Append('.');
+        return builder.ToString();
+    }
+
+    private static string CreateName(string? owner)
+    {
+        StringBuilder builder = new(41);
+        builder.Append("The ");
+        if (owner != null)
+        {
+            builder.Append($"{owner}'s ");
+        }
+        builder.Append("phone number");
+        return builder.ToString();
     }
 }
