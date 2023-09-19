37e11a778bc4ab7f2004da58ce7a3cd692a4f421
Andrej Križan <andrej_so@hotmail.com>
2023-09-19 20:59:01 +0200

    Hdf: modernized HdfType dynamic type detection/creation, and added support for bool type.

--- Hdf/Entities/Types/HdfType.cs
+++ Hdf/Entities/Types/HdfType.cs
@@ -48,43 +48,42 @@ public class HdfType<T> : HdfObject, IHdfType<T>
         => HdfConstants.ResponseSuccessCode;
 
     // Private methods
-    private static long GetTypeId(Type type) => type switch
-    {
-        Type _type when _type == typeof(byte) => H5T.NATIVE_B8,
-        Type _type when _type == typeof(short) => H5T.NATIVE_INT16,
-        Type _type when _type == typeof(ushort) => H5T.NATIVE_UINT16,
-        Type _type when _type == typeof(int) => H5T.NATIVE_INT32,
-        Type _type when _type == typeof(uint) => H5T.NATIVE_UINT32,
-        Type _type when _type == typeof(long) => H5T.NATIVE_INT64,
-        Type _type when _type == typeof(ulong) => H5T.NATIVE_UINT64,
-        Type _type when _type == typeof(float) => H5T.NATIVE_FLOAT,
-        Type _type when _type == typeof(double) => H5T.NATIVE_DOUBLE,
-        Type _type when _type == typeof(char) => H5T.NATIVE_CHAR,
-        Type _type when _type == typeof(string) => throw new NotSupportedException($"When using a string type, use the {nameof(HdfStringType)} method instead."),
-        Type _type when _type == typeof(DateTime) => throw new NotSupportedException($"When using a datetime type, use the {nameof(HdfStringType)} method instead."),
-        _ => throw new NotSupportedException($"Unsupported type: {type}."),
-    };
+    private static long GetTypeId(Type type)
+        => type switch
+        {
+            _ when type == typeof(bool) => H5T.NATIVE_HBOOL,
+            _ when type == typeof(byte) => H5T.NATIVE_B8,
+            _ when type == typeof(short) => H5T.NATIVE_INT16,
+            _ when type == typeof(ushort) => H5T.NATIVE_UINT16,
+            _ when type == typeof(int) => H5T.NATIVE_INT32,
+            _ when type == typeof(uint) => H5T.NATIVE_UINT32,
+            _ when type == typeof(long) => H5T.NATIVE_INT64,
+            _ when type == typeof(ulong) => H5T.NATIVE_UINT64,
+            _ when type == typeof(float) => H5T.NATIVE_FLOAT,
+            _ when type == typeof(double) => H5T.NATIVE_DOUBLE,
+            _ when type == typeof(char) => H5T.NATIVE_CHAR,
+            _ when type == typeof(string) => throw new NotSupportedException($"When using a string type, use the {nameof(HdfStringType)} method instead."),
+            _ when type == typeof(DateTime) => throw new NotSupportedException($"When using a datetime type, use the {nameof(HdfStringType)} method instead."),
+            _ => throw new NotSupportedException($"Unsupported type: {type}."),
+        };
 
     private static byte[] ConvertToBytes(T value)
-    {
-        object valueObject = value;
-        byte[] bytes = typeof(T) switch
+        => value switch
         {
-            Type _type when _type == typeof(byte) => new byte[] { (byte)valueObject },
-            Type _type when _type == typeof(short) => BitConverter.GetBytes((short)valueObject),
-            Type _type when _type == typeof(ushort) => BitConverter.GetBytes((ushort)valueObject),
-            Type _type when _type == typeof(int) => BitConverter.GetBytes((int)valueObject),
-            Type _type when _type == typeof(uint) => BitConverter.GetBytes((uint)valueObject),
-            Type _type when _type == typeof(long) => BitConverter.GetBytes((long)valueObject),
-            Type _type when _type == typeof(ulong) => BitConverter.GetBytes((ulong)valueObject),
-            Type _type when _type == typeof(Half) => BitConverter.GetBytes((Half)valueObject),
-            Type _type when _type == typeof(float) => BitConverter.GetBytes((float)valueObject),
-            Type _type when _type == typeof(double) => BitConverter.GetBytes((double)valueObject),
-            Type _type when _type == typeof(char) => BitConverter.GetBytes((char)valueObject),
-            Type _type when _type == typeof(string) => throw new NotSupportedException($"When using a string type, use the {nameof(HdfStringType)} instead."),
-            Type _type when _type == typeof(DateTime) => throw new NotSupportedException($"When using a datetime type, use the {nameof(HdfDateTimeType)} instead."),
+            bool _value => BitConverter.GetBytes(_value),
+            byte _value => new byte[] { _value },
+            short _value => BitConverter.GetBytes(_value),
+            ushort _value => BitConverter.GetBytes(_value),
+            int _value => BitConverter.GetBytes(_value),
+            uint _value => BitConverter.GetBytes(_value),
+            long _value => BitConverter.GetBytes(_value),
+            ulong _value => BitConverter.GetBytes(_value),
+            Half _value => BitConverter.GetBytes(_value),
+            float _value => BitConverter.GetBytes(_value),
+            double _value => BitConverter.GetBytes(_value),
+            char _value => BitConverter.GetBytes(_value),
+            string _value => throw new NotSupportedException($"When using a string type, use the {nameof(HdfStringType)} instead."),
+            DateTime _value => throw new NotSupportedException($"When using a datetime type, use the {nameof(HdfDateTimeType)} instead."),
             _ => throw new NotSupportedException($"The type {typeof(T)} is not supported.")
         };
-        return bytes;
-    }
 }
