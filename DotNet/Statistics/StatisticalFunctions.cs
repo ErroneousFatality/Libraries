dffc0d8a19beef6a539f4583188b0425381d662f
Andrej Križan <andrej_so@hotmail.com>
2023-10-23 01:57:24 +0200

    DotNet: Statistical - added SampleUniques better documentation and validation exceptions.

--- DotNet/Statistics/StatisticalFunctions.cs
+++ DotNet/Statistics/StatisticalFunctions.cs
@@ -2,8 +2,17 @@
 public static class StatisticalFunctions
 {
     /// <summary>Works in θ(size) time and space.</summary>
+    /// <param name="size">Must be non-negative.</param>
     public static HashSet<int> SampleUniques(int size, int min, int max)
     {
+        if (size < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(size), "The size must be a non-negative integer.");
+        }
+        if (min > max - size)
+        {
+            throw new ArgumentException("The range of available values (max - min) must can not be less than size.");
+        }
         Random generator = new();
         HashSet<int> uniques = new(size);
         for (int index = max - size + 1; index <= max; index++)
