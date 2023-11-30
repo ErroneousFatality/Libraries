64189adaf9d987124df117794b610d543f34f91c
Andrej Križan <andrej_so@hotmail.com>
2023-11-30 11:04:52 +0100

    [v5.2.0] SqlClient: added configuration methods so that the user doesn't have to know how to configure the SqlClientRepository into the DI.

---
+++ SqlClient/Configuration/IServiceCollectionExtensions.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AndrejKrizan.SqlClient.Configuration;
+public static class IServiceCollectionExtensions
+{
+    public static IServiceCollection AddSqlClientRepository(this IServiceCollection services, string connectionString)
+        => services.AddTransient<SqlClientRepository>(_ => new SqlClientRepository(connectionString));
+}
