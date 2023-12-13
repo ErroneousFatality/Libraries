b896ff0e651b9b6f3b2c59dbbd43d0bc4d13b178
Andrej Križan <andrej_so@hotmail.com>
2023-12-13 17:08:46 +0100

    [v5.5.1] DotNet: Entity bugfixes.

--- DotNet/Entities/Entity.cs
+++ DotNet/Entities/Entity.cs
@@ -1,19 +1,18 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace AndrejKrizan.DotNet.Entities;
+﻿namespace AndrejKrizan.DotNet.Entities;
 public abstract class Entity<TId>
 {
     // Properties
-    public required TId Id { get; init; }
+    public TId Id { get; init; }
 
     // Constructors
 
-    [SetsRequiredMembers]
     protected Entity(TId id)
     {
         Id = id;
     }
 
     /// <summary>Used for EntityFrameworkCore deserialization.</summary>
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     protected Entity() { }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 }
