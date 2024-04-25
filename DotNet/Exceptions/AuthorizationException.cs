af09a3f3ea00bb14bd1dd53454a60b9a516bedf0
Andrej Križan <andrej_so@hotmail.com>
2024-04-25 16:06:45 +0200

    [v8.1.1] DotNet.Exceptions:   - AuthorizationException: added constructor to which you can pass just an inner exception   - NotFoundException: replaced all constructors with one which has all the parameters as optional.

--- DotNet/Exceptions/AuthorizationException.cs
+++ DotNet/Exceptions/AuthorizationException.cs
@@ -3,11 +3,13 @@
 public class AuthorizationException : InvalidOperationException
 {
     // Constructors
-
     public AuthorizationException() { }
 
     public AuthorizationException(string message)
         : base(message) { }
+
+    public AuthorizationException(Exception innerException)
+        : base(null, innerException) { }
 
     public AuthorizationException(string message, Exception innerException)
         : base(message, innerException) { }
--- DotNet/Exceptions/NotFoundException.cs
+++ DotNet/Exceptions/NotFoundException.cs
@@ -2,18 +2,10 @@
 public class NotFoundException : ArgumentException
 {
     // Constructors
-
-    public NotFoundException() { }
-
-    public NotFoundException(string message)
-        : base(message) { }
-
-    public NotFoundException(string message, Exception innerException)
-        : base(message, innerException) { }
-
-    public NotFoundException(string message, string paramName)
-        : base(message, paramName) { }
-
-    public NotFoundException(string message, string paramName, Exception innerException)
+    public NotFoundException(
+        string? paramName = null, 
+        string? message = null, 
+        Exception? innerException = null
+    )
         : base(message, paramName, innerException) { }
 }
