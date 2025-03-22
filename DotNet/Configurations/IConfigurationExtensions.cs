87c5490d277c1cc5105e34be1296908ec5970802
Andrej Križan <andrej_so@hotmail.com>
2025-03-22 13:55:28 +0100

    [9.0.7] DotNet: Configurations -> GetRequiredValue  - changed default behaviour to throw on empty and whitespace strings, but added an optional parameter which allows configuring the valid string states Elasticsearch: updated underlying library package.

--- DotNet/Configurations/IConfigurationExtensions.cs
+++ DotNet/Configurations/IConfigurationExtensions.cs
@@ -5,14 +5,35 @@ namespace AndrejKrizan.DotNet.Configurations;
 public static class IConfigurationExtensions
 {
     /// <exception cref="ArgumentException">The value was not found inside the configuration.</exception>
-    public static string GetRequiredValue(this IConfiguration configuration, string key)
-        => configuration[key]
-            ?? throw new ArgumentException($"The \"{key}\" value was not found inside the configuration.", nameof(configuration));
+    public static string GetRequiredValue(this IConfiguration configuration, string key, StringState minimalState = StringState.Populated)
+    {
+        string? value = configuration[key];
+        if (IsStringInvalid(value, minimalState))
+        {
+            throw new ArgumentException($"The \"{key}\" value was not found inside the configuration.", nameof(configuration));
+        }
+        return value!;
+    }
 
     /// <exception cref="ArgumentException">The value was not found inside the configuration section.</exception>
-    public static string GetRequiredValue(this IConfigurationSection configurationSection, string key)
-        => configurationSection[key]
-            ?? throw new ArgumentException($"The \"{key}\" value was not found inside the configuration section at path \"{configurationSection.Path}\".", nameof(configurationSection));
+    public static string GetRequiredValue(this IConfigurationSection configurationSection, string key, StringState minimalState = StringState.Populated)
+    {
+        string? value = configurationSection[key];
+        if (IsStringInvalid(value, minimalState))
+        {
+            throw new ArgumentException($"The \"{key}\" value was not found inside the configuration section at path \"{configurationSection.Path}\".", nameof(configurationSection));
+        }
+        return value!;
+    }
+
+    private static bool IsStringInvalid(string? value, StringState minimalState)
+        => minimalState switch
+        {
+            StringState.Empty => value == null,
+            StringState.Whitespace => string.IsNullOrEmpty(value),
+            StringState.Populated => string.IsNullOrWhiteSpace(value),
+            _ => throw new ArgumentOutOfRangeException(nameof(minimalState))
+        };
 
 
     /// <exception cref="ArgumentException">The settings were not found or are invalid inside the configuration.</exception>
---
+++ DotNet/Configurations/StringState.cs
@@ -0,0 +1,7 @@
+﻿namespace AndrejKrizan.DotNet.Configurations;
+public enum StringState : byte
+{
+    Empty = 1,
+    Whitespace = 2,
+    Populated = 3
+}
