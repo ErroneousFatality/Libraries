bce5cc0e9f6058e3f997fe49d192a0e9d108a902
Andrej Križan <andrej_so@hotmail.com>
2026-02-04 21:29:47 +0100

    [v10.2.3.0] DotNet.Net.Http: now extends the HttpResponseMessage class with methods GetFileName() and GetMediaType(). ElasticSearch, PhoneNumbers, SqlClient: updated underyling libraries.

---
+++ DotNet/Net/Http/HttpResponseMessageExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+
+namespace AndrejKrizan.DotNet.Net.Http;
+
+public static class HttpResponseMessageExtensions
+{
+    extension(HttpResponseMessage responseMessage)
+    {
+        /// <exception cref="ArgumentException"></exception>
+        public string GetFileName()
+        {
+         ContentDispositionHeaderValue contentDisposition = responseMessage.Content.Headers.ContentDisposition
+                ?? throw new ArgumentException("The HTTP response message is missing the content disposition header.", nameof(responseMessage));
+            string fileName = contentDisposition.FileNameStar
+                ?? WebUtility.UrlDecode(contentDisposition.FileName)?.Replace("\"", string.Empty)
+                ?? throw new ArgumentException("The HTTP response message is missing the file name values inside the disposition header.", nameof(responseMessage));
+            return fileName;
+        }
+
+        /// <exception cref="ArgumentException"></exception>
+        public string GetMediaType()
+        {
+            MediaTypeHeaderValue contentTypeHeader = responseMessage.Content.Headers.ContentType
+                ?? throw new ArgumentException("The HTTP response message is missing the content type header.", nameof(responseMessage));
+            string mediaType = contentTypeHeader.MediaType
+                ?? throw new ArgumentException("The HTTP response message is missing the media type value inside the content type header.", nameof(responseMessage));
+            return mediaType;
+        }
+    }
+}
